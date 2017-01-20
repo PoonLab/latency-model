79c06d07a1bc2432279ccbb2c55853c8d00eef54
Art Poon <artpoon@gmail.com>
2017-01-20 00:04:50 -0500

    fixes #2

--- scripts/read-nexus.py
+++ scripts/read-nexus.py
@@ -1,6 +1,7 @@
 from Bio import Phylo
 import re
 from StringIO import StringIO
+import sys
 
 pat = re.compile('\[\&([^,\]]+),')
 
@@ -14,14 +15,6 @@ while matches:
     text = re.sub(pat, '[&\\1 ', text)
     matches = re.findall(pat, text)
 
-# check for balanced parentheses
 
-
-try:
-    tree = Phylo.read(StringIO(text), 'nexus')
-except:
-    raise
-
-#tree = Phylo.read('RPTree_output.nexus', 'nexus')
-
+tree = Phylo.read(StringIO(text), 'nexus')
 
